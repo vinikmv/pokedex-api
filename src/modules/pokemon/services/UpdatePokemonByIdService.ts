b38c6f2f9d66daf482fb836185f4ed147ea62557
viniciuskmv <viniciuskmv@gmail.com>
2020-11-09 20:36:34 -0300

    feat: add UpdatePokemonByIdService

---
+++ src/modules/pokemon/services/UpdatePokemonByIdService.ts
@@ -0,0 +1,42 @@
+import AppError from '@shared/infra/errors/AppErros';
+import { inject, injectable } from 'tsyringe';
+import Pokemon from '../infra/typeorm/entities/Pokemon';
+import IPokemonRepository from '../IRepositories/IPokemonRepository';
+
+interface IRequest {
+  id: string;
+  name: string;
+  type: string;
+  abilities: string[];
+  moves: string[];
+}
+
+@injectable()
+class UpdatePokemonByIdService {
+  constructor(
+    @inject('PokemonRepository') private pokemonRepository: IPokemonRepository,
+  ) {}
+
+  public async execute({
+    id,
+    name,
+    type,
+    abilities,
+    moves,
+  }: IRequest): Promise<Pokemon> {
+    const pokemon = await this.pokemonRepository.findById(id);
+
+    if (!pokemon) {
+      throw new AppError('O pokemon não existe.');
+    }
+
+    pokemon.name = name;
+    pokemon.type = type;
+    pokemon.abilities = abilities;
+    pokemon.moves = moves;
+
+    return this.pokemonRepository.updatePokemon(pokemon);
+  }
+}
+
+export default UpdatePokemonByIdService;
