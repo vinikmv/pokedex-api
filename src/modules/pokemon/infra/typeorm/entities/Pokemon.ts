717a47ec967b0bdeb0423cab2d20a8b63b3a5510
viniciuskmv <viniciuskmv@gmail.com>
2020-11-09 20:32:05 -0300

    feat: add Pokemon Entity

--- src/modules/pokemon/infra/typeorm/entities/Pokemon.ts
+++ src/modules/pokemon/infra/typeorm/entities/Pokemon.ts
@@ -1,4 +1,23 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 @Entity('pokemon')
-export default class Pokemon {}
+export default class Pokemon {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  type: string;
+
+  @Column({
+    type: 'simple-array',
+  })
+  abilities: string[];
+
+  @Column({
+    type: 'simple-array',
+  })
+  moves: string[];
+}
