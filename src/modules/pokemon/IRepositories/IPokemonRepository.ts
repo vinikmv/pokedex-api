fb03a7c84aeb02bee09c5dc3dd2dfc5265676264
viniciuskmv <viniciuskmv@gmail.com>
2020-11-09 20:32:34 -0300

    feat: add IPokemonRepository

--- src/modules/pokemon/IRepositories/IPokemonRepository.ts
+++ src/modules/pokemon/IRepositories/IPokemonRepository.ts
@@ -1,1 +1,10 @@
-export default interface IPokemonRepository {}
+import ICreatePokemonDTO from '../dtos/ICreatePokemonDTO';
+import Pokemon from '../infra/typeorm/entities/Pokemon';
+
+export default interface IPokemonRepository {
+  create(pokemonData: ICreatePokemonDTO): Promise<Pokemon>;
+  findById(id: string): Promise<Pokemon | undefined>;
+  getAllPokemon(): Promise<Pokemon[]>;
+  updatePokemon(pokemonData: Pokemon): Promise<Pokemon>;
+  deletePokemon(pokemonData: Pokemon): Promise<void>;
+}
