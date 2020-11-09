822ecf197afe28df9288f9bd497f157e146475b0
viniciuskmv <viniciuskmv@gmail.com>
2020-11-09 20:36:07 -0300

    feat: add DeletePokemonByIdService

---
+++ src/modules/pokemon/services/DeletePokemonByIdService.ts
@@ -0,0 +1,24 @@
+import AppError from '@shared/infra/errors/AppErros';
+import { inject, injectable } from 'tsyringe';
+import IPokemonRepository from '../IRepositories/IPokemonRepository';
+
+interface IRequest {
+  id: string;
+}
+
+@injectable()
+class DeletePokemonByIdService {
+  constructor(
+    @inject('PokemonRepository') private pokemonRepository: IPokemonRepository,
+  ) {}
+
+  public async execute({ id }: IRequest): Promise<void> {
+    const pokemon = await this.pokemonRepository.findById(id);
+    if (!pokemon) {
+      throw new AppError('O pokemon não existe.');
+    }
+    await this.pokemonRepository.deletePokemon(pokemon);
+  }
+}
+
+export default DeletePokemonByIdService;
