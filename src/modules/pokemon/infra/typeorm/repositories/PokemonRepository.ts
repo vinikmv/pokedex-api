19469b7adaf156d4841c12fd310253862c43d786
viniciuskmv <viniciuskmv@gmail.com>
2020-11-09 20:33:03 -0300

    feat: add PokemonRepository

--- src/modules/pokemon/infra/typeorm/repositories/PokemonRepository.ts
+++ src/modules/pokemon/infra/typeorm/repositories/PokemonRepository.ts
@@ -1,3 +1,4 @@
+import ICreatePokemonDTO from '@modules/pokemon/dtos/ICreatePokemonDTO';
 import IPokemonRepository from '@modules/pokemon/IRepositories/IPokemonRepository';
 import { getRepository, Repository } from 'typeorm';
 import Pokemon from '../entities/Pokemon';
@@ -7,5 +8,34 @@ export default class PokemonRepository implements IPokemonRepository {
 
   constructor() {
     this.ormRepository = getRepository(Pokemon);
+  }
+
+  async create(pokemonData: ICreatePokemonDTO): Promise<Pokemon> {
+    const pokemon = this.ormRepository.create(pokemonData);
+    await this.ormRepository.save(pokemon);
+    return pokemon;
+  }
+
+  async findById(id: string): Promise<Pokemon | undefined> {
+    const pokemon = await this.ormRepository.findOne(id);
+    return pokemon;
+  }
+
+  async getAllPokemon(): Promise<Pokemon[]> {
+    const pokemon = await this.ormRepository
+      .createQueryBuilder('pokemon')
+      .getMany();
+
+    return pokemon;
+  }
+
+  async updatePokemon(pokemonData: Pokemon): Promise<Pokemon> {
+    const updatedPokemon = await this.ormRepository.save(pokemonData);
+
+    return updatedPokemon;
+  }
+
+  async deletePokemon(pokemonData: Pokemon): Promise<void> {
+    await this.ormRepository.remove(pokemonData);
   }
 }
