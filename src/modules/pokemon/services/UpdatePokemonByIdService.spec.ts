b27fc1389bc8cd785fd05da37ca03e1c32ccb273
viniciuskmv <viniciuskmv@gmail.com>
2020-11-10 13:34:34 -0300

    test: add validation when updating pokemon data

--- src/modules/pokemon/services/UpdatePokemonByIdService.spec.ts
+++ src/modules/pokemon/services/UpdatePokemonByIdService.spec.ts
@@ -1,14 +1,20 @@
 import AppError from '@shared/infra/errors/AppError';
 import FakePokemonRepository from '../IRepositories/fake/FakePokemonRepository';
+import FakeValidationProvider from '../providers/validation/fake/FakeValidationProvider';
 import UpdatePokemonByIdService from './UpdatePokemonByIdService';
 
 let fakePokemonRepository: FakePokemonRepository;
+let fakeValidationProvider: FakeValidationProvider;
 let updatePokemon: UpdatePokemonByIdService;
 
 describe('DeletePokemon', () => {
   beforeEach(() => {
     fakePokemonRepository = new FakePokemonRepository();
-    updatePokemon = new UpdatePokemonByIdService(fakePokemonRepository);
+    fakeValidationProvider = new FakeValidationProvider();
+    updatePokemon = new UpdatePokemonByIdService(
+      fakePokemonRepository,
+      fakeValidationProvider,
+    );
   });
 
   test('Should be able to update a pokemon ', async () => {
@@ -47,5 +53,24 @@ describe('DeletePokemon', () => {
     });
 
     await expect(deletedPokemon).rejects.toBeInstanceOf(AppError);
+  });
+
+  test('Should call AppError if empty field is provided ', async () => {
+    await fakePokemonRepository.create({
+      id: '1',
+      name: 'PokemonTest',
+      type: 'normal',
+      abilities: ['a1', 'a2', 'a3'],
+      moves: ['m1', 'm2'],
+    });
+
+    const invalidPokemon = updatePokemon.execute({
+      id: '1',
+      name: '',
+      type: 'fire',
+      abilities: ['b', 'b2', 'b3'],
+      moves: ['n1', 'n2'],
+    });
+    await expect(invalidPokemon).rejects.toBeInstanceOf(AppError);
   });
 });
