65a92ce06ebb591436af768bc044bd8d74f4147b
viniciuskmv <viniciuskmv@gmail.com>
2020-11-10 13:34:26 -0300

    feat: add validation when updating pokemon data

--- src/modules/pokemon/services/UpdatePokemonByIdService.ts
+++ src/modules/pokemon/services/UpdatePokemonByIdService.ts
@@ -2,6 +2,7 @@ import AppError from '@shared/infra/errors/AppError';
 import { inject, injectable } from 'tsyringe';
 import Pokemon from '../infra/typeorm/entities/Pokemon';
 import IPokemonRepository from '../IRepositories/IPokemonRepository';
+import IValidationProvider from '../providers/validation/models/IValidationProvider';
 
 interface IRequest {
   id: string;
@@ -15,6 +16,8 @@ interface IRequest {
 class UpdatePokemonByIdService {
   constructor(
     @inject('PokemonRepository') private pokemonRepository: IPokemonRepository,
+    @inject('ValidateFieldProvider')
+    private validateProvider: IValidationProvider,
   ) {}
 
   public async execute({
@@ -29,6 +32,12 @@ class UpdatePokemonByIdService {
     if (!pokemon) {
       throw new AppError('O pokemon não existe.');
     }
+
+    const fieldArray = [id, name, type];
+    const field2Array = [abilities, moves];
+
+    fieldArray.forEach(this.validateProvider.validateString);
+    field2Array.forEach(this.validateProvider.validateArray);
 
     pokemon.name = name;
     pokemon.type = type;
